9a204ab876d88bb89ef1c22e002103e6391b82d5
Zhengqiang Liao <lzq666amn@gmail.com>
2025-03-27 23:01:23 +0800

    First Commit

---
+++ algorithms/src/iofile.rs
@@ -0,0 +1,18 @@
+use std::fs::File;
+use std::io::{self, Read, Result};
+
+fn process_file() -> Result<()> {
+    Ok(())
+}
+
+fn block_process() {
+    
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {}
+}
